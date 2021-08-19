348ae75ea8ba1bd883eaa3046d7a70b3ce3e1f28
lohannahme <61636785+lohannahme@users.noreply.github.com>
2021-08-19 18:53:46 -0300

    Files

---
+++ testeaula/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testeaula
+{
+    class Conta
+    {
+        public int Nconta { get; private set; }
+        public string Nome { get; private set; }
+        public double Vinicial { get; private set; }
+
+        public Conta(int nconta, string nome)
+        {
+            Nconta = nconta;
+            Nome = nome;
+            Vinicial = 0;
+        }
+
+        public Conta(int nconta, string nome, double vinicial) : this(nconta, nome)
+        {
+            Vinicial = vinicial;
+        }
+
+        public void Deposito(double dep)
+        {
+            Vinicial += dep;
+        }
+
+        public void Saque(double saq)
+        {
+            Vinicial -= saq+5;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Nconta}, Titular: {Nome}, Saldo: $ {Vinicial.ToString("F2")}. ";
+        }
+    }
+}
---
+++ testeaula/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace testeaula
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int n;
+            double med = 0;
+
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine());
+                med += vet[i];
+            }
+
+            med = med / n;
+
+            Console.WriteLine($"AVERAGE HEIGHT = {med.ToString("F2")}");
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
